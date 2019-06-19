7d76e3c41d55fd41e0a6bad74a691e2c692b32aa
Rafael Montenegro <rafaelgsmontenegro@gmail.com>
2019-06-18 19:11:42 -0700

    Wk1 4-6 solutions.

--- Day4-Day6/wk1_d4-d6_callbacks.js
+++ Day4-Day6/wk1_d4-d6_callbacks.js
@@ -1,4 +1,4 @@
-// Start a new branch, and switch to that branch. Make all your changes on that branch, and then do a pull request to merge the branch to your main branch. Don't know how to do that? Let me know, I will tell you how. 
+// Start a new branch, and switch to that branch. Make all your changes on that branch, and then do a pull request to merge the branch to your main branch. Don't know how to do that? Let me know, I will tell you how.
 
 // If you don't know how to run this quickly, you can always just do it in a repl.it window. Never used repl??!! 😲 🤪 🤭 Well, get on it, my friend. Google repl, or just go to the website repl.it. Just copy over your answer to this doc when you're done.
 
@@ -7,32 +7,57 @@
 // Stretch: Make your console display “Ready…”, then “Set…”, then “Go!”, one second after another.
 
 
-// a quick note: 👇 you can't use the arrow functions for callbacks. You have to use the full word. Bummer, I know... 
+// a quick note: 👇 you can't use the arrow functions for callbacks. You have to use the full word. Bummer, I know...
+
+console.log("Ready...");
 
 setTimeout (function(){
-    
-    // Your Code Here...
 
-}, /* milliseconds */);
+    console.log("Set...");
 
+    setTimeout (function(){
 
-// 2. Make a counter using setInterval. Count up from 1, every second. 
-
-// Stretch: Create two counters, one for even numbers, one for odd. Run them at the same time. 
-
-setInterval(function(){
-
-    // Your Code Here...
-
-}, /* milliseconds */);
+        console.log("Go!");
 
 
-// 3. Use setInterval to log “ . “ to the console every second. Stop the logging after 10 seconds have passed. 
-// Hint: use setTimeout to stop it. 
+    }, 1500);
 
-// Stretch: Build a blast-off countdown using setInterval. 
+}, 1500);
+
+
+// 2. Make a counter using setInterval. Count up from 1, every second.
+
+// Stretch: Create two counters, one for even numbers, one for odd. Run them at the same time.
+var counterEven = 0;
+var counterOdd = 1;
+
+var x = setInterval(function(){
+
+    counterEven = counterEven + 2;
+    console.log("Even = " + counterEven);
+
+    counterOdd = counterOdd + 2;
+    console.log("Odd = " + counterOdd);
+
+}, 1000);
+
+
+// 3. Use setInterval to log “ . “ to the console every second. Stop the logging after 10 seconds have passed.
+// Hint: use setTimeout to stop it.
+
+// Stretch: Build a blast-off countdown using setInterval.
 // 10 9 8 7 6 5 4 3 2 1 Blast Off!!
 
-const interval = setInterval(function(){},/* milliseconds */);
+var counter = 10;
 
+const interval = setInterval(function(){
+    if(counter == 0){
+        console.log("Blast Off!!");
+        clearInterval(interval);
+    } else {
+        console.log(""+counter);
+    }
+    
+    counter--;
 
+}, 1000);
